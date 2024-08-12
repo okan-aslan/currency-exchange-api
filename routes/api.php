9716604687ba6c81ec8e61306bc54439a85b749d
Okan Aslan <aaslan.okan@gmail.com>
2024-08-12 14:29:12 +0300

    added accounts routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [AuthController::class, 'profile']);
     Route::post('profile/logout', [AuthController::class, 'logout']);
     Route::post('profile/delete', [AuthController::class, 'destroy']);
+
+    Route::get('accounts', [AccountController::class, 'getAllAccounts']);
+    Route::post('accounts', [AccountController::class, 'createAccount']);
+    Route::get('/accounts/{accountNo}/convert/{targetCurrency}', [AccountController::class, 'getAccountValue']);
+    Route::post('accounts/{accountNo}/deposit', [AccountController::class, 'deposit']);
+    Route::post('accounts/{accountNo}/withdraw', [AccountController::class, 'withdraw']);
+    Route::get('accounts/{accountNo}', [AccountController::class, 'showAccount']);
+    Route::delete('accounts/{accountNo}', [AccountController::class, 'deleteAccount']);
 });
