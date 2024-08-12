34f5cbe31b7d9ef16415bf3c3cecf2b802e0a7d0
Okan Aslan <aaslan.okan@gmail.com>
2024-08-12 13:08:06 +0300

    Created Account Model & Added User Model Relation & Added Soft Delete

---
+++ app/Models/Account.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Account extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * Get the user associated with the Account
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+}
