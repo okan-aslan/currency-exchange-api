0441efb22c5078d85541d204c454957ebdbf7aab
Okan Aslan <aaslan.okan@gmail.com>
2024-08-12 14:30:37 +0300

    added method to generate random 12 chars unique account_no

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -6,10 +6,31 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Account extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = ['currency'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($account) {
+            $account->account_no = self::generateUniqueAccountNo();
+        });
+    }
+
+    private static function generateUniqueAccountNo()
+    {
+        do {
+            $accountNo = Str::upper(Str::random(12));
+        } while (self::where('account_no', $accountNo)->exists());
+
+        return $accountNo;
+    }
 
     /**
      * Get the user associated with the Account
