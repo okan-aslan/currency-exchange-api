a7168a4c27ea9137738a8ce36f1a5bed23eae08a
Okan Aslan <aaslan.okan@gmail.com>
2024-08-12 12:46:54 +0300

    Added Auth Routes

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [AuthController::class, 'profile']);
+    Route::post('profile/logout', [AuthController::class, 'logout']);
+    Route::post('profile/delete', [AuthController::class, 'destroy']);
+});
