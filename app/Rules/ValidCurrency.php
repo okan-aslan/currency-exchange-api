9771056ea482bd9dfae4cce6d7636f6ad846e173
Okan Aslan <aaslan.okan@gmail.com>
2024-08-16 22:04:43 +0300

    New rule created to obtain a currency type suitable for the API.

---
+++ app/Rules/ValidCurrency.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Rules;
+
+use App\Services\ExchangeRateService;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ValidCurrency implements ValidationRule
+{
+    protected $supportedCurrencies;
+
+    public function __construct(ExchangeRateService $exchangeRateService)
+    {
+        $this->supportedCurrencies = $exchangeRateService->getSupportedCurrencies();
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!in_array(strtoupper($value), $this->supportedCurrencies)) {
+            $fail('The selected currency is not supported.');
+        }
+    }
+}
