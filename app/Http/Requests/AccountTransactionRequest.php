f3f11bfd4a4eb0fdaf5f66dc9f9f92611f82f806
Okan Aslan <aaslan.okan@gmail.com>
2024-08-18 13:25:17 +0300

    added authorization to request

--- app/Http/Requests/AccountTransactionRequest.php
+++ app/Http/Requests/AccountTransactionRequest.php
@@ -7,6 +7,15 @@ use Illuminate\Foundation\Http\FormRequest;
 class AccountTransactionRequest extends FormRequest
 {
     /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->accounts()->where('account_no', $this->input('account_no'))->exists();
+    }
+
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
