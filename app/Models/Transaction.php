89ceb59e0d861cb69283766d0b15f42ea9c98eb0
Okan Aslan <aaslan.okan@gmail.com>
2024-08-18 12:31:01 +0300

    created model & added relations, softdelete and $fillable

---
+++ app/Models/Transaction.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    protected $fillable = ['amount', 'type'];
+
+    /**
+     * Get the user that performs the transaction
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the account that owns the Transaction
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    /**
+     * Get the user that owns the Transaction
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function targetAccount(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
